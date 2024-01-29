104909476fd139a78e73f70336ed2d99da5a7af5
Pavel Kogan <pashaweb@gmail.com>
2024-01-29 11:17:05 +0200

    Composition added

--- apps/react-cripto-game/src/app/app.tsx
+++ apps/react-cripto-game/src/app/app.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import ChartHolder from './components/ChartHolder';
 import { CurrencyTrader } from './components/CurrencyTrader';
 import { useCryptoStore, initStore, CryptoNames } from './store/store';
 import { useEffect } from 'react';
@@ -8,6 +9,8 @@ export function App() {
 
   const {
     moneyData,
+    currentPrices,
+    cryptoMap,
     buy,
     sell,
   } = useCryptoStore();
@@ -48,7 +51,16 @@ export function App() {
       <div className={styles['fake-masonry']}>
         {
           [...Object.entries(moneyData.coins)].map((coin, i) => {
+            const name = coin[0] as CryptoNames;
+            const props = {
+              name,
+              currentPrice: currentPrices.get(name) ? currentPrices.get(name) : 0,
+              chartData: cryptoMap.get(name) ? cryptoMap.get(name) : [],
+            }
             return (
+
+
+
 
 
               <CurrencyTrader
@@ -57,7 +69,23 @@ export function App() {
                 coins={coin[1]}
                 buyCrypto={handeleBuyCrypto}
                 sellCrypto={handleSellCrypto}
-              />
+              >
+                <ChartHolder
+                  name={props.name}
+                  chartData={props.chartData}
+                  currentPrice={props.currentPrice || 0} />
+
+                <div>
+                  <label>Dolar Value:</label>
+                  <br /> <b>
+                    $ {(props.currentPrice || 0) * coin[1]}
+                  </b>
+                </div>
+
+
+              </CurrencyTrader>
+
+
 
             )
           })
